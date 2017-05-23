4913beb1db807846b81c67f778ea9065eafbca37
z4p <z4p@tut.by>
2017-05-23 16:50:30 +0300

    html-gen fixes

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function CellularAutomaton(domId) {
 CellularAutomaton.prototype = {
     width: 32,
     height: 32,
-	maxStateValue: 4,
+	maxStateValue: 2,
     history: [],
     loop: 0,
     ticks: 0,
@@ -20,14 +20,6 @@ CellularAutomaton.prototype = {
     currentRule: null,
     saveEnabled: false,
     saves: [],
-    
-    btnStep: null,
-    btnReset: null,
-    btnGo: null,
-    btnSave: null,
-    btnRestore: null,
-    listSaves: null,
-    txtInfo: null,
     
     restore: function() {
         if(!this.saveEnabled) {
@@ -47,19 +39,19 @@ CellularAutomaton.prototype = {
             return;
         }
         
-        $(this.listSaves).empty();
+        $('#states').empty();
         for(var i = 0; i < this.saves.length; i++) {
-            $("<option value=" + i + ">" + this.saves[i].name + "</option>").appendTo(this.listSaves);
+            $("<option value=" + i + ">" + this.saves[i].name + "</option>").appendTo( $('#states') );
         }
     },
     
     loadState: function() {
-        if(this.listSaves.value === "") {
+        if($('#states').val() == "") {
             alert("Не выбрано состояние для загрузки");
             return;
         }
         
-        var strSave = this.saves[this.listSaves.value].state;
+        var strSave = this.saves[$('#states').val()].state;
         
         var c = 0;
         for(var i = 0; i < this.height; i++) {
@@ -72,18 +64,18 @@ CellularAutomaton.prototype = {
     },
     
 	eraseState: function() {
-		if(this.listSaves.value === "") {
+		if($('#states').val() === "") {
             alert("Не выбрано состояние для удаления");
             return;
         }
-        var i = this.listSaves.value;
+        var i = $('#states').val();
         
 		this.saves.splice(i, 1);
         this.save();
 		
-		$(this.listSaves).empty();
+		$('#states').empty();
         for(var i = 0; i < this.saves.length; i++) {
-            $("<option value=" + i + ">" + this.saves[i].name + "</option>").appendTo(this.listSaves);
+            $("<option value=" + i + ">" + this.saves[i].name + "</option>").appendTo($('#states'));
         }
 	},
 	
@@ -106,9 +98,9 @@ CellularAutomaton.prototype = {
         
         this.saves.push( newSave );
         
-        $(this.listSaves).empty();
+        $('#saves').empty();
         for(var i = 0; i < this.saves.length; i++) {
-            $("<option value=" + i + ">" + this.saves[i].name + "</option>").appendTo(this.listSaves);
+            $("<option value=" + i + ">" + this.saves[i].name + "</option>").appendTo($('#states'));
         }
         
         if(this.saveEnabled) {
@@ -143,150 +135,119 @@ CellularAutomaton.prototype = {
             }
         }
         
-        // таблица-матрица для отрисовки состояния
-        var tbl = document.createElement('table');
-        tbl.id = 'cellArea';
-        $('.area').append(tbl);
-        this.DOMTable = tbl;
+        // таблица-матрица для отрисовки состояний
+        this.DOMTable = $('#cellArea')[0];
         
-        // input ввода числа шагов
-        var inpStep = $('<input id="stepCount" value=1 size=7>');
-        this.inpStep = inpStep;
         
         // форма управления
-        var btnStep = document.createElement('BUTTON');
-        btnStep.appendChild( document.createTextNode('шаги') );
-        btnStep.onclick = function() {
-            that.steps = parseInt($("#stepCount").val());
-            that.goSteps();
-            btnGo.innerHTML = 'Стоп';
-        };
-        this.btnStep = btnStep;
+		
+        $('#btnSteps').click(function() {
+			if(that.timer) {
+                that.stopSteps();
+                $('#btnStart').text('Старт');
+            } else {
+                that.steps = parseInt($("#stepCount").val());
+                that.goSteps();
+                $('#btnSteps').text('Стоп');
+            }
+        });
         
-        var btnReset = document.createElement('BUTTON');
-        btnReset.appendChild( document.createTextNode('Сброс') );
-        btnReset.onclick = function() {
+        $('#btnClear').click(function() {
+            for(var i = 0; i < that.height; i++) {
+				for(var j = 0; j < that.width; j++) {
+					that.a[i][j] = 0;
+				}
+			}
+			that.redraw();
+        });
+        
+        $('#btnFill').click(function() {
+            for(var i = 0; i < that.height; i++) {
+				for(var j = 0; j < that.width; j++) {
+					that.a[i][j] = 1;
+				}
+			}
+			that.redraw();
+        });
+        
+        $('#btnReset').click(function() {
             that.clearArea();
-        };
-        this.btnReset = btnReset;
+        });
         
-        var btnGo = document.createElement('BUTTON');
-        btnGo.appendChild( document.createTextNode('Старт') );
-        btnGo.onclick = function() {
+		/*
+        $('#btnStart').click(function() {
             if(that.timer) {
                 that.stopSteps();
-                btnGo.innerHTML = 'Старт';
+                $('#btnStart').text('Старт');
             } else {
                 that.steps = 100500;
                 that.goSteps();
-                btnGo.innerHTML = 'Стоп';
+                $('#btnStart').text('Стоп');
             }
-        };
-        this.btnGo = btnGo;
+        });
+		*/
         
-        var btnSave = document.createElement('BUTTON');
-        btnSave.appendChild( document.createTextNode('Сохранить') );
-        btnSave.onclick = function() {
+        $('#btnSave').click(function() {
             that.saveState();
-        };
-        this.btnSave = btnSave;
+        });
         
-        var btnRestore = document.createElement('BUTTON');
-        btnRestore.appendChild( document.createTextNode('Загрузить') );
-        btnRestore.onclick = function() {
+        $('#btnRestore').click(function() {
             that.loadState();
-        };
-        this.btnRestore = btnRestore;
+        });
         
-        var btnErase = document.createElement('BUTTON');
-        btnErase.appendChild( document.createTextNode('Удалить') );
-        btnErase.onclick = function() {
+        $('#btnDelete').click(function() {
             that.eraseState();
-        };
-        this.btnErase = btnErase;
-        
-        var btnRand = document.createElement('BUTTON');
-        btnRand.appendChild( document.createTextNode('Бульон') );
-        btnRand.onclick = function() {
+        });
+
+        $('#btnSoup').click(function() {
             that.randomSoup();
-        };
-        this.btnRand = btnRand;
+        });
         
-        var btnNoise = document.createElement('BUTTON');
-        btnNoise.appendChild( document.createTextNode('Шум') );
-        btnNoise.onclick = function() {
+        $('#btnNoise').click(function() {
             that.noise();
             that.redraw();
-        };
-        this.btnNoise = btnNoise;
+        });
         
-        var listSaves = document.createElement('SELECT');
-        listSaves.setAttribute('size', 8);
-		listSaves.ondblclick = function() {
+        $('#states').dblclick(function() {
 			that.loadState();
-		}
-        this.listSaves = listSaves;
+		});
         
-        
-        
-        // выбор правил
         
         this.rules = [this.ruleSum, this.ruleBurn, this.ruleLife, this.ruleCycle, this.ruleSuCA];
         this.currentRule = this.rules[0];
         
-        var listRules = document.createElement('SELECT');
-		$("<option value='0'>Сумма (mod 2)</option>").appendTo( listRules );
-		$("<option value='1'>Голосование</option>").appendTo( listRules );
-		$("<option value='2'>Игра 'Жизнь'</option>").appendTo( listRules );
-		$("<option value='3'>Циклический</option>").appendTo( listRules );
-        $("<option value='4'>КА окрестность фон неймана</option>").appendTo( listRules );
-		listRules.onchange = function() {
+        $('#rule').change(function() {
 			that.currentRule = that.rules[$(this).val()];
-		}
-        this.listRules = listRules;
+			console.log($(this).val());
+		});
         
-        
-        // выбор периода
-        var timerSelect = document.createElement('SELECT');
-        $("<option value='100'>100 мс</option>").appendTo( timerSelect );
-		$("<option value='200'>200 мс</option>").appendTo( timerSelect );
-		$("<option value='500'>500 мс</option>").appendTo( timerSelect );
-		$("<option value='1000'>1000 мс</option>").appendTo( timerSelect );
-		timerSelect.onchange = function() {
+		$('#delay').change(function() {
 			that.timeT = $(this).val();
-		}
-        this.timerSelect = timerSelect;
+		});
         
-        // выбор размеров поля
-        var sizeSelect = document.createElement('SELECT');
-        $("<option value='8'>8x8</option>").appendTo( sizeSelect );
-		$("<option value='16'>16x16</option>").appendTo( sizeSelect );
-		$("<option value='32' selected>32x32</option>").appendTo( sizeSelect );
-		$("<option value='64'>64x64</option>").appendTo( sizeSelect );
-		$("<option value='other'>другой размер</option>").appendTo( sizeSelect );
-		sizeSelect.onchange = function() {
+		$('#statecount').change(function() {
+			that.maxStateValue = $(this).val();
+		});
+        
+		$('#size').change(function() {
             if ($(this).val() === 'other') {
                 that.width = prompt("Ширина поля","32");
                 that.height = prompt("Высота поля","32");
-                that.a = new Array(that.height);
-                for(var i = 0; i < that.height; i++) {
-                    that.a[i] = new Array(that.width);
-                    for(var j = 0; j < that.width; j++) {
-                        that.a[i][j] = 0;
-                    }
-                }
             } else {
                 that.width = $(this).val();
                 that.height = $(this).val();
             }
+			
+			that.a = new Array(that.height);
+			for(var i = 0; i < that.height; i++) {
+				that.a[i] = new Array(that.width);
+				for(var j = 0; j < that.width; j++) {
+					that.a[i][j] = 0;
+				}
+			}
+			
             that.redraw();
-		}
-        this.sizeSelect = sizeSelect;
-        
-        
-        var info = document.createElement('P');
-        info.setAttribute('id', 'caInfo');
-        this.txtInfo = info;
+		});
         
         // проверка работы хранилища
         if(typeof(Storage) !== "undefined") {
@@ -295,54 +256,15 @@ CellularAutomaton.prototype = {
             alert("Ваш браузер не поддерживает работу с локальным хранилищем.\n"
             +"Функции сохранения и загрузки состояний недоступны!");
             
-            btnSave.setAttribute('disabled','disabled');
-            btnRestore.setAttribute('disabled','disabled');
-            listSaves.setAttribute('disabled','disabled');
+            $('#btnSave').attr('disabled','disabled');
+            $('#btnLoad').attr('disabled','disabled');
+            $('#states').attr('disabled','disabled');
         }
         
-        var ctrl = document.getElementById('control');
-        var br = function(){return document.createElement('BR');}
-        var hr = function(){return document.createElement('HR');}
-        
-        ctrl.appendChild(sizeSelect);
-        ctrl.appendChild(br());
-        ctrl.appendChild(timerSelect);
-        ctrl.appendChild(br());
-        inpStep.appendTo(ctrl);
-        ctrl.appendChild(btnStep);
-        ctrl.appendChild(br());
-        ctrl.appendChild(btnGo);
-        ctrl.appendChild(btnReset);
-        ctrl.appendChild(hr());
-        ctrl.appendChild(btnRand);
-        ctrl.appendChild(btnNoise);
-        ctrl.appendChild(hr());
-        ctrl.appendChild(listRules);
-        ctrl.appendChild(hr());
-        ctrl.appendChild(listSaves);
-        ctrl.appendChild(br());
-        ctrl.appendChild(btnSave);
-        ctrl.appendChild(br());
-        ctrl.appendChild(btnErase);
-        ctrl.appendChild(btnRestore);
-        ctrl.appendChild(hr());
-        ctrl.appendChild(info);
-        
-        // восстановление сохраненных состояний из localStorage
         this.restore();
-        
         this.redraw();
     },
-    /*
-    check: function(i,j) {
-        var c = 0;
-        if (i > 0) c += this.a[i-1][j];
-        if (j > 0) c += this.a[i][j-1];
-        if (i < this.height-1) c += this.a[i+1][j];
-        if (j < this.width-1)  c += this.a[i][j+1];
-        return c;
-    },
-    */
+    
     ruleSum: function(i,j) {
         var c = 0;
         for(var a = i-1; a <= i+1; a++)
@@ -376,34 +298,31 @@ CellularAutomaton.prototype = {
     },
     
     ruleCycle: function(i,j) {
-        var c1 = 0;
-        var c2 = 0;
-        for(var a = i-1; a <= i+1; a++)
-            for(var b = j-1; b <= j+1; b++)
-                if (a >= 0 && b >= 0 && a < this.height && b < this.width && !(a == i && b == j)) {
-                    if(this.a[a][b] === 1) c1++;
-                    else c2++;
-                }
-        if (this.a[i][j] === 0 && c1 > 0) return 1;
-        if (this.a[i][j] === 1 && c2 > 0) return 0;
+        var c = 0;
+        if (i > 0)              c += this.a[i-1][j];
+        if (j > 0)              c += this.a[i][j-1];
+        if (i < this.height-1)  c += this.a[i+1][j];
+        if (j < this.width-1)   c += this.a[i][j+1];
+
+        if (c % 2) return (this.a[i][j] + 1) % this.maxStateValue;
         return this.a[i][j];
     },
     
     ruleSuCA: function(i,j) {
         var c = this.a[i][j];
         
-        if (i > 0)              c ^= this.a[i-1][j];
-        if (j > 0)              c ^= this.a[i][j-1];
-        if (i < this.height-1)  c ^= this.a[i+1][j];
-        if (j < this.width-1)   c ^= this.a[i][j+1];
+        if (i > 0)              c += this.a[i-1][j];
+        if (j > 0)              c += this.a[i][j-1];
+        if (i < this.height-1)  c += this.a[i+1][j];
+        if (j < this.width-1)   c += this.a[i][j+1];
                 
-        return c;
+        return c % this.maxStateValue;
     },
     
     noise: function() {
         for(var i = 0; i < this.height; i++) {
-            for(var j = 0; j < this.height; j++) {
-                if (Math.random() > 0.9) {
+            for(var j = 0; j < this.width; j++) {
+                if (Math.random() > 0.95) {
                     this.a[i][j] = (this.a[i][j] + 1) % this.maxStateValue;
                 }
             }
@@ -411,11 +330,7 @@ CellularAutomaton.prototype = {
     },
     
     clearArea: function() {
-        for(var i = 0; i < this.height; i++) {
-            for(var j = 0; j < this.height; j++) {
-                this.a[i][j] = 0;
-            }
-        }
+		this.history = [];
         this.ticks = 0;
         this.loop = 0;
         this.redraw();
@@ -459,7 +374,7 @@ CellularAutomaton.prototype = {
 
         if (l.length == 0) { // if nothing changed
             if (this.timer) {
-                this.btnGo.click();
+                $('#btnSteps').click();
             }
             this.loop = 1;
             //alert("Устойчивое состояние\n(состояние тривиального цикла)");
@@ -479,14 +394,17 @@ CellularAutomaton.prototype = {
 		var ticks_text = "Тактов: "+this.ticks;
 		
         if (this.loop !== 0) {
-            $( this.txtInfo ).text("Длина цикла: "+this.loop+"\n"+ticks_text);
+            $('#caInfo').text("Длина цикла: "+this.loop+"\n"+ticks_text);
         } else {
-            $( this.txtInfo ).text(ticks_text);
+            $('#caInfo').text(ticks_text);
         }
         for(var i = 0; i < this.height; i++) {
             var tRow = document.createElement('TR');
             for(var j = 0; j < this.width; j++) {
                 var tCell = document.createElement('TD');
+				if (isNaN(this.a[i][j])) {
+					this.a[i][j] = 0;
+				}
                 tCell.className = 'state' + this.a[i][j];
                 tCell.setAttribute('rowNum', i);
                 tCell.setAttribute('colNum', j);
@@ -497,7 +415,6 @@ CellularAutomaton.prototype = {
 						that.a[i][j] = 0;
 					}
                     that.a[i][j] = (that.a[i][j] + 1) % that.maxStateValue;
-                    //                    console.log(i, j, that.a[i][j]);
                     that.redraw();
                 };
                 var nbsp = document.createTextNode('');
@@ -524,7 +441,7 @@ CellularAutomaton.prototype = {
     
     stopSteps: function() {
         clearInterval(this.timer);
-        this.btnGo.innerHTML = 'Старт';
+		$('#btnSteps').text('Старт');
         this.timer = null;
     },
 };
