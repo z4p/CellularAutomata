49fcb29e3871f931aadda29764d811cfebc1c1e7
z4p <z4p@tut.by>
2020-06-18 22:18:28 +0300

    color transitions

--- js/zclock.js
+++ js/zclock.js
@@ -4,6 +4,13 @@ class ZCloudteamClock {
         this.clock_hh = this.clock_root.querySelector('.clock__hh');
         this.clock_mm = this.clock_root.querySelector('.clock__mm');
 
+        this.colorHSV = {
+            h: 0,
+            s: 0,
+            v: 1,
+        };
+        this.color_iterator = 0;
+
         const self = this;
 
         this.clock_root.addEventListener('touchstart', (e) => {
@@ -17,7 +24,10 @@ class ZCloudteamClock {
         });
 
         setInterval(function() {
+            self.colorHSV.h = Math.sin(self.color_iterator)/2 + 0.5;
+            self.colorHSV.s = Math.sin(self.color_iterator * 1.5)/2 + 0.5;
             self.redraw();
+            self.color_iterator += 0.01;
         }, 1000);
     }
 
@@ -32,6 +42,7 @@ class ZCloudteamClock {
             minutes = '0' + minutes;
         }
 
+        this.clock_root.style.color = ZCloudteamClock.HSVtoRGB(this.colorHSV);
         this.clock_hh.innerHTML = hours.toString();
         this.clock_mm.innerHTML = minutes.toString();
     }
@@ -44,5 +55,24 @@ class ZCloudteamClock {
                 document.exitFullscreen();
             }
         }
+    }
+
+    static HSVtoRGB(hsv) {
+        let s = hsv.s, v = hsv.v, h = hsv.h;
+        let r, g, b, i, f, p, q, t;
+        i = Math.floor(h * 6);
+        f = h * 6 - i;
+        p = v * (1 - s);
+        q = v * (1 - f * s);
+        t = v * (1 - (1 - f) * s);
+        switch (i % 6) {
+            case 0: r = v, g = t, b = p; break;
+            case 1: r = q, g = v, b = p; break;
+            case 2: r = p, g = v, b = t; break;
+            case 3: r = p, g = q, b = v; break;
+            case 4: r = t, g = p, b = v; break;
+            case 5: r = v, g = p, b = q; break;
+        }
+        return 'rgb(' + Math.round(r * 255) + ',' + Math.round(g * 255) + ',' + Math.round(b * 255) + ')';
     }
 }
